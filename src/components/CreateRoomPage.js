d08f1d15385f31fd37dac1773c9585451736e73c
Sheltey01 <janek.hoericht@gmx.net>
2021-03-25 22:20:01 +0100

    da

--- src/components/CreateRoomPage.js
+++ src/components/CreateRoomPage.js
@@ -148,7 +148,7 @@ export default class CreateRoomPage extends Component{
                     }}
                   />
                   <FormHelperText>
-                    <div align="center">The amount of players which is required</div>
+                    <div align="center">The amount of players which are required</div>
                   </FormHelperText>
                   <TextField
                     required={true}
